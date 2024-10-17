a77f4a83add17846744ebd11149c8b03f00bd79d
Red Importante <redimportante13@gmail.com>
2024-10-17 12:22:01 +0800

    added logging and continue training from best weights

--- train.py
+++ train.py
@@ -2,6 +2,7 @@
 import os
 import time
 import argparse
+import logging
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -24,10 +25,8 @@ from floortrans.loaders.augmentations import (RandomCropToSizeTorch,
                                               ColorJitterTorch,
                                               RandomRotations)
 
-# Model
+# Own
 from model.deeplabv3plus import DeepLabV3Plus
-
-# Multi-task Uncertainty Loss
 from loss_fn import MultiTaskUncertaintyLoss
 
 # Release GPU memory
@@ -41,7 +40,8 @@ else:
     device = torch.device('cpu')
     print('Using device: CPU')
 
-# Parse command line arguments
+
+# Parse command line arguments for setting hyperparameters
 parser = argparse.ArgumentParser(description='Train DeepLabV3+ on the CubiCasa5k dataset')
 parser.add_argument('--img_size', type=int, default=256, help='Size to resize the images (default: 256)')
 parser.add_argument('--bs', type=int, default=32, help='Batch size (default: 32)')
@@ -50,9 +50,10 @@ parser.add_argument('--backbone', type=str, default='mobilenetv2', help='Backbon
 parser.add_argument('--attention', action='store_true', help='Use CA and SA modules (default: False)')
 parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train (default: 100)')
 parser.add_argument('--lr', type=float, default=0.001, help='Initial learning rate (default: 0.001)')
+parser.add_argument('--reload_best_model', action='store_true', help='Resume training from best model checkpoint (default: False)')
+
 args = parser.parse_args()
 
-# Set hyperparameters from args
 IMAGE_SIZE = (args.img_size, args.img_size)
 BATCH_SIZE = args.bs
 NUM_WORKERS = args.workers
@@ -60,14 +61,16 @@ BACKBONE = args.backbone
 USE_ATTENTION = args.attention
 EPOCHS = args.epochs
 INITIAL_LR = args.lr
+RELOAD_BEST_MODEL = args.reload_best_model
+
 
 # Preprocessing and Augmentations
 train_aug = Compose([
-    transforms.RandomChoice([
-        RandomCropToSizeTorch(data_format='dict', size=IMAGE_SIZE),
-        ResizePaddedTorch((0, 0), data_format='dict', size=IMAGE_SIZE)
-    ]),
-    # ResizePaddedTorch((0, 0), data_format='dict', size=IMAGE_SIZE),
+    # transforms.RandomChoice([
+    #     RandomCropToSizeTorch(data_format='dict', size=IMAGE_SIZE),
+    #     ResizePaddedTorch((0, 0), data_format='dict', size=IMAGE_SIZE)
+    # ]),
+    ResizePaddedTorch((0, 0), data_format='dict', size=IMAGE_SIZE),
     RandomRotations(format='cubi'),
     DictToTensor(),
     ColorJitterTorch(b_var=0.2, c_var=0.2, s_var=0.2)
@@ -125,30 +128,45 @@ val_loader = DataLoader(
 # print('\nBatch image shape: ', batch_sample['image'].shape)
 # print('Batch label shape: ', batch_sample['label'].shape)
 
+
 # Model Setup
 model = DeepLabV3Plus(backbone=BACKBONE, attention=USE_ATTENTION).to(device)
+
+# summary(model, input_size=(BATCH_SIZE, 3, IMAGE_SIZE[0], IMAGE_SIZE[1]))
 
 if USE_ATTENTION:
     print(f'Model Loaded: DeepLabV3+ with {BACKBONE} backbone and CA + SA modules\n')
 else:
     print(f'Model Loaded: DeepLabV3+ with {BACKBONE} backbone and no attention modules\n')
 
-# summary(model, input_size=(BATCH_SIZE, 3, IMAGE_SIZE[0], IMAGE_SIZE[1]))
 
-# TRAINING AND VALIDATION LOOP
-# Individual loss functions for each task
+# Loss functions
 room_criterion = nn.CrossEntropyLoss()
 icon_criterion = nn.CrossEntropyLoss()
 heatmap_criterion = nn.MSELoss()
-
-# Multi-task uncertainty loss
 multitask_criterion = MultiTaskUncertaintyLoss(num_tasks=3).to(device)
 
-# Based on CubiCasa5k training setup
+# These are based on CubiCasa5k training setup
 optimizer = optim.Adam(model.parameters(), lr=INITIAL_LR, betas=(0.9, 0.999), eps=1e-8)
-
-# Reduce learning rate if validation loss doesnt improve for 20 epochs
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=20)
+
+
+# Load best model checkpoint if available or train from scratch
+checkpoint_path = f'saved_models/best_checkpoint_{model.backbone_name}_{model.attention}.pt'
+
+if RELOAD_BEST_MODEL and os.path.exists(checkpoint_path):
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    best_val_loss = checkpoint['best_val_loss']
+    start_epoch = checkpoint['epoch'] + 1
+    print(f"Resuming training from epoch {start_epoch} with best validation loss {best_val_loss:.4f}\n")
+else:
+    best_val_loss = float('inf')
+    start_epoch = 0
+    print("Starting training from scratch\n")
+
 
 # Evaluation metrics
 room_metrics = MetricCollection({
@@ -164,118 +182,107 @@ icon_metrics = MetricCollection({
 }).to(device)
 
 
-# Training loop
 def train(model, dataloader, optimizer, device):
     model.train()
-
     total_loss = 0.0
 
-    # To show progress bar
     for batch in tqdm(dataloader, desc='Training'):
         images = batch['image'].float().to(device)
         room_labels = batch['label'][:, 21].long().to(device)
         icon_labels = batch['label'][:, 22].long().to(device)
         heatmap_labels = batch['label'][:, 0:21].to(device)
 
-        # Zero out gradients
         optimizer.zero_grad()
 
         # Forward pass
         room_output, icon_output, heatmap_output = model(images)
 
-        # Compute individual losses per task
+        # Compute individual losses then feed into multi-task loss
         room_loss = room_criterion(room_output, room_labels)
         icon_loss = icon_criterion(icon_output, icon_labels)
         heatmap_loss = heatmap_criterion(heatmap_output, heatmap_labels)
 
-        # Compute combined loss
         combined_loss = multitask_criterion([room_loss, icon_loss, heatmap_loss])
 
         # Backward pass and optimization
         combined_loss.backward()
         optimizer.step()
 
-        # Accumulate the scalar value of the loss
         total_loss += combined_loss.item()
 
         # Get model predictions
         room_preds = torch.argmax(room_output, dim=1)
         icon_preds = torch.argmax(icon_output, dim=1)
 
-        # Update metrics
+        # By batch
         room_metrics.update(room_preds, room_labels)
         icon_metrics.update(icon_preds, icon_labels)
 
-    # Compute epoch loss and metrics
+    # By epoch
     epoch_loss = total_loss / len(dataloader)
     epoch_room_metrics = room_metrics.compute()
     epoch_icon_metrics = icon_metrics.compute()
 
-    # Reset metrics
     room_metrics.reset()
     icon_metrics.reset()
 
     return epoch_loss, epoch_room_metrics, epoch_icon_metrics
 
 
-# Validation loop
 def validate(model, dataloader, device):
     model.eval()
-
     total_loss = 0.0
 
-    # Don't compute gradients during validation
     with torch.no_grad():
         for batch in tqdm(dataloader, desc='Validating'):
             images = batch['image'].float().to(device)
-
             room_labels = batch['label'][:, 21].long().to(device)
             icon_labels = batch['label'][:, 22].long().to(device)
             heatmap_labels = batch['label'][:, 0:21].to(device)
 
-            # Forward pass
             room_output, icon_output, heatmap_output = model(images)
 
-            # Compute individual losses
             room_loss = room_criterion(room_output, room_labels)
             icon_loss = icon_criterion(icon_output, icon_labels)
             heatmap_loss = heatmap_criterion(heatmap_output, heatmap_labels)
-
-            # Compute combined loss
             combined_loss = multitask_criterion([room_loss, icon_loss, heatmap_loss])
 
-            # Accumulate the scalar value of the loss
             total_loss += combined_loss.item()
 
-            # Get model predictions
             room_preds = torch.argmax(room_output, dim=1)
             icon_preds = torch.argmax(icon_output, dim=1)
 
-            # Update metrics
             room_metrics.update(room_preds, room_labels)
             icon_metrics.update(icon_preds, icon_labels)
     
-    # Compute epoch loss and metrics
     epoch_loss = total_loss / len(dataloader)
     epoch_room_metrics = room_metrics.compute()
     epoch_icon_metrics = icon_metrics.compute()
 
-    # Reset metrics
     room_metrics.reset()
     icon_metrics.reset()
     
     return epoch_loss, epoch_room_metrics, epoch_icon_metrics
 
 
-# Train and validate model
 if __name__ == '__main__':
-    best_val_loss = float('inf')
+    # Setup logging
+    log_dir = 'logs'
+    os.makedirs(log_dir, exist_ok=True)
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    log_filename = f"{model.backbone_name}_{model.attention}_{timestamp}.log"
+    log_filepath = os.path.join(log_dir, log_filename)
+
+    logging.basicConfig(filename=log_filepath, level=logging.INFO,format='%(asctime)s - %(message)s')
+    logging.info(f'Logging training for DeepLabV3+ with {BACKBONE} backbone and attention: {USE_ATTENTION}\n')
+
+    # Setup for training and validation
     lr_dropped = False
     early_stopping_patience = 30
     early_stopping_counter = 0
-    model_path = f'saved_models/deeplabv3plus_{model.backbone_name}_{model.attention}.pt'
 
-    for epoch in range(EPOCHS):
+    for epoch in range(start_epoch, EPOCHS):
         print(f"EPOCH {epoch+1}/{EPOCHS}")
 
         train_loss, train_room_metrics, train_icon_metrics = train(model, train_loader, optimizer, device)
@@ -297,30 +304,47 @@ if __name__ == '__main__':
         val_icon_miou = val_icon_metrics['miou'].item()
         val_icon_fwiou = val_icon_metrics['fwiou'].item()
 
-        print('\nTRAIN RESULTS')
-        print(f'Loss: {train_loss:.4f}')
-        print(f'Rooms - mPA: {train_room_mpa:.4f}, mIoU: {train_room_miou:.4f}, fwIoU: {train_room_fwiou:.4f}')
-        print(f'Icons - mPA: {train_icon_mpa:.4f}, mIoU: {train_icon_miou:.4f}, fwIoU: {train_icon_fwiou:.4f}')
-
-        print('\nVALIDATION RESULTS')
-        print(f'Loss: {val_loss:.4f}')
-        print(f'Rooms - mPA: {val_room_mpa:.4f}, mIoU: {val_room_miou:.4f}, fwIoU: {val_room_fwiou:.4f}')
-        print(f'Icons - mPA: {val_icon_mpa:.4f}, mIoU: {val_icon_miou:.4f}, fwIoU: {val_icon_fwiou:.4f}')
+        log_message = (
+            f"\nTRAIN RESULTS\n"
+            f"Loss: {train_loss:.4f}\n"
+            f"Rooms - mPA: {train_room_mpa:.4f}, mIoU: {train_room_miou:.4f}, fwIoU: {train_room_fwiou:.4f}\n"
+            f"Icons - mPA: {train_icon_mpa:.4f}, mIoU: {train_icon_miou:.4f}, fwIoU: {train_icon_fwiou:.4f}\n"
+            f"\nVALIDATION RESULTS\n"
+            f"Loss: {val_loss:.4f}\n"
+            f"Rooms - mPA: {val_room_mpa:.4f}, mIoU: {val_room_miou:.4f}, fwIoU: {val_room_fwiou:.4f}\n"
+            f"Icons - mPA: {val_icon_mpa:.4f}, mIoU: {val_icon_miou:.4f}, fwIoU: {val_icon_fwiou:.4f}\n"
+        )
+        logging.info(log_message)
+        print(log_message)
 
         # Save best model
         if val_loss < best_val_loss:
             best_val_loss = val_loss
-            lr_dropped = False
             early_stopping_counter = 0
-            torch.save(model.state_dict(), model_path)
-            print(f'\nBest model saved with validation loss: {best_val_loss:.4f}')
+
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'scheduler_state_dict': scheduler.state_dict(),
+                'best_val_loss': best_val_loss
+            }, checkpoint_path)
+
+            save_msg = f'\nBest model saved with validation loss: {best_val_loss:.4f}'
+            print(save_msg)
+            logging.info(save_msg)
+
         else:
             early_stopping_counter += 1
-            print(f'\nValidation loss did not improve.Best loss is still: {best_val_loss:.4f}')
+            not_improved_msg = f'\nValidation loss did not improve. Best loss is still: {best_val_loss:.4f}'
+            print(not_improved_msg)
+            logging.info(not_improved_msg)
 
-        # Terminate if model didn't improve for the early stopping patience
+        # Activate early stopping if no improvement for specific epochs
         if early_stopping_counter >= early_stopping_patience:
-            print(f'\nEarly stopping triggered after {epoch+1} epochs')
+            early_stop_msg = f'\nEarly stopping triggered after {epoch+1} epochs'
+            print(early_stop_msg)
+            logging.info(early_stop_msg)
             break
 
         # Check if LR was dropped
@@ -331,7 +355,11 @@ if __name__ == '__main__':
         # Reload best model weights if LR was dropped
         if current_lr < prev_lr and not lr_dropped:
             lr_dropped = True
-            model.load_state_dict(torch.load(model_path))
-            print(f'\nLR dropped to {current_lr}. Model weights reloaded with best validation loss: {best_val_loss:.4f}')
+            model.load_state_dict(torch.load(checkpoint_path)['model_state_dict'])
+            torch.cuda.empty_cache()
+
+            reload_best_msg = f'\nLR dropped to {current_lr}. Model weights reloaded with best validation loss: {best_val_loss:.4f}'
+            print(reload_best_msg)
+            logging.info(reload_best_msg)
 
         print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
