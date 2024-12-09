c51311acd3a8f79e16d72cf2d44275ef16a43252
Annalurhge <annalurhge@gmail.com>
2024-12-09 18:47:41 +0800

    mwahahaha

--- tool/ui/MainInterface.py
+++ tool/ui/MainInterface.py
@@ -23,15 +23,15 @@ class MainInterface():
         # self.run_both_models = False
         self.show_core_elements_only = False
 
-        self.output = None
-        self.labels = None
+        self.output = []
+        self.labels = []
 
         self.root = root
 
         red = 'C:/Users/Red/Documents/GitHub/deeplabv3plus_cubicasa/data/cubicasa5k/'
-        me = ''
+        me = 'D:\GitHub\deepl_lab\data\cubicasa5k\\'
 
-        # self.dataset = load_dataset(red)
+        self.dataset = load_dataset(me)
         
         main_frame = self._initializeMainFrame(root)
         self._loadContents(main_frame);
@@ -63,7 +63,7 @@ class MainInterface():
         show_segmentation_maps_btn = CTkButton(main_frame, text="Show Segmentation Maps", text_color="black", font=CTkFont("Arial", 12, weight="bold"))
         show_segmentation_maps_btn.configure(corner_radius=32,
                                             image=self._createCtkImage(self.IMAGE_PATHS.MAP_ICON, size=(12, 12)),
-                                            command=lambda: self.Renderer.show_segmentation_maps())
+                                            command=self._seeSegMaps)
         show_segmentation_maps_btn.place(relx=0.28, rely=0.865, relwidth=0.4, anchor="center")
         
         model_renderer_frame = CTkButton(main_frame)
@@ -97,27 +97,11 @@ class MainInterface():
                                               text="Show only core floor plan elements")
         show_core_elements_only.configure(command=lambda: [self._setParameter("showCoreElementsOnly", show_core_elements_only.get())])
         show_core_elements_only.place(relx=0.5, rely=0.95, anchor="center")
-        
-        # use_both_models = CTkCheckBox(main_frame,
-        #                               text="Run both models at the same time?")
-        # use_both_models.configure(command=lambda: [select_model.configure(state="disabled" if use_both_models.get() else "normal"), self._setParameter("useBothModels", use_both_models.get())])
-        # use_both_models.place(relx=0.5, rely=0.95, anchor="center")
-
-        # compare_btn = CTkButton(main_frame, 
-        #                         text="Compare", 
-        #                         text_color="black", 
-        #                         font=CTkFont("Arial", 12, weight="bold"),
-        #                         image=self._createCtkImage(self.IMAGE_PATHS.COMPARE_IMAGE_ICON, size=(12, 12)),
-        #                         width=120, height=30,
-        #                         corner_radius=32)
-        # compare_btn.place(relx=0.81, rely=0.95, anchor="center")
     
     def _createCtkImage(self, image_path, size):
         return CTkImage(Image.open(image_path), size=size)
     
     def _uploadPhoto(self, button_widget):
-        # selected_floorplan = filedialog.askopenfilename(title="Select a floorplan image", filetypes=[("Image Files", "*.png *.jpg *.jpeg")])
-
         def pop_file(filepath):
             path, _ = os.path.split(filepath)
             return path
@@ -156,25 +140,107 @@ class MainInterface():
         
         print(f"Selected {parameter}: {value}")
     
+    def _seeSegMaps(self):
+        model_output_path = r"D:\GitHub\deepl_lab\tool\deeplab\floorplan_pred512.pt"
+        model_output_path1 = r"D:\GitHub\deepl_lab\tool\deeplab\floorplan_pred512.pt"
+        
+        model_outputs = [model_output_path, model_output_path1]
+        # ic(torch.load(model_output_path))
+        self.root.seeSegMaps(model_outputs)
+        # self.root.seeDetails(self.output, self.labels)
+        
     def _seeDetails(self):
-        self.root.seeDetails(self.output, self.labels)
+        metrics =[
+                    ["Background", "0.98"],
+                    ["Outdoor", "0.96"],
+                    ["Wall", "0.94"],
+                    ["Kitchen", "0.97"],
+                    ["Living room", "0.92"],
+                    ["Bedroom", "0.94"],
+                    ["Bath", "0.95"],
+                    ["Hallway", "0.99"],
+                    ["Railing", "0.92"],
+                    ["Storage", "0.90"],
+                    ["Garage", "0.97"],
+                    ["Other rooms", "0.92"],
+                    ["Empty", "0.99"],
+                    ["Window", "0.98"],
+                    ["Door", "0.95"],
+                    ["Closet", "0.99"],
+                    ["Electr. Appl.", "0.97"],
+                    ["Toilet", "0.90"],
+                    ["Sink", "0.96"],
+                    ["Sauna bench", "0.90"],
+                    ["Fire Place", "0.93"],
+                    ["Bathtub", "0.94"],
+                    ["Chimney", "0.94"],
+                    ["Other", "0.90"],
+                    ["Mean Pixel Accuracy", "0.97"],
+                    ["Mean IoU", "0.99"],
+                    ["Frequency-Weighted IoU", "0.98"]
+                ]
+        metrics1 =[
+                    ["Background", "0.98"],
+                    ["Outdoor", "0.96"],
+                    ["Wall", "0.94"],
+                    ["Kitchen", "0.97"],
+                    ["Living room", "0.92"],
+                    ["Bedroom", "0.94"],
+                    ["Bath", "0.95"],
+                    ["Hallway", "0.99"],
+                    ["Railing", "0.92"],
+                    ["Storage", "0.90"],
+                    ["Garage", "0.97"],
+                    ["Other rooms", "0.92"],
+                    ["Empty", "0.99"],
+                    ["Window", "0.98"],
+                    ["Door", "0.95"],
+                    ["Closet", "0.99"],
+                    ["Electr. Appl.", "0.97"],
+                    ["Toilet", "0.90"],
+                    ["Sink", "0.96"],
+                    ["Sauna bench", "0.90"],
+                    ["Fire Place", "0.93"],
+                    ["Bathtub", "0.94"],
+                    ["Chimney", "0.94"],
+                    ["Other", "0.90"],
+                    ["Mean Pixel Accuracy", "0.97"],
+                    ["Mean IoU", "0.99"],
+                    ["Frequency-Weighted IoU", "0.98"]
+                ]
+        metric_outputs = [metrics, metrics1]
+        self.root.seeDetails(metric_outputs)
 
     def _generateModel(self):
         device = "cuda" if torch.cuda.is_available() else "cpu"
-
-        model = load_model(
-            f"C:/Users/Red/Documents/GitHub/deeplabv3plus_cubicasa/tool/deeplab/best_checkpoint_mobilenetv2_{'base' if self.model_to_use == 0 else 'ca_sa'}.pt", 
-            use_attention=False if self.model_to_use == 0 else True, device=device)
-        
-        print(f"Model loaded: deeplabv3plus_{model.backbone_name}_{model.attention}")
-
         img, labels = load_img_and_labels(self.dataset, self.selected_floorplan)
-        # print(img.shape, labels.shape)
-
-        combined_tensor, metrics_output = evaluate(model, img, labels)
         
-        self.labels = combined_tensor
-        self.output = metrics_output
+        models = []
+        
+        metrics = []
+        outputs = []
+
+        if self.model_to_use < 2:
+            model = load_model(
+                f"C:/Users/Red/Documents/GitHub/deeplabv3plus_cubicasa/tool/deeplab/best_checkpoint_mobilenetv2_{'base' if self.model_to_use == 0 else 'ca_sa'}.pt", 
+                use_attention=False if self.model_to_use == 0 else True, device=device)
+            combined_tensor, metrics_output = evaluate(model, img, labels)
+            metrics.append(combined_tensor)
+            outputs.append(metrics_output)
+        else:
+            for i in range(2):
+                model = load_model(
+                    f"C:/Users/Red/Documents/GitHub/deeplabv3plus_cubicasa/tool/deeplab/best_checkpoint_mobilenetv2_{'base' if i == 0 else 'ca_sa'}.pt", 
+                    use_attention=False if i == 0 else True, device=device)
+                models.append(model)
+                combined_tensor, metrics_output = evaluate(model, img, labels)
+                metrics.append(combined_tensor)
+                outputs.append(metrics_output)
+                print(f"Model loaded: deeplabv3plus_{model.backbone_name}_{model.attention}")
+
+        
+        self.labels = metrics
+        self.output = outputs
 
         # self.Vectorizer(combined_tensor)
         # sample = torch.load(r"C:\Users\Red\Documents\GitHub\deeplabv3plus_cubicasa\tool\deeplab\floorplan_pred256.pt")
